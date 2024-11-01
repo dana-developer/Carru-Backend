2d9690225d430b7cc649e3173b3550e9d070554d
DANA <75533232+dana-developer@users.noreply.github.com>
2024-11-01 20:22:20 +0900

    chore:spring boot 초기설정

---
+++ src/main/java/capstone/carru/CarruApplication.java
@@ -0,0 +1,13 @@
+package capstone.carru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/carru/CarruApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.carru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
