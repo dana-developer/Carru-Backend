58ef096291bc35fb0ac6ced2eaa2af334d2d0e56
HanDaYeon-coder <hdy78901@naver.com>
2024-11-02 13:36:17 +0900

    chore: exception 관련 설정 사항 추가

---
+++ src/main/java/capstone/carru/exception/NotFoundException.java
@@ -0,0 +1,18 @@
+package capstone.carru.exception;
+
+import capstone.carru.driver.dto.ErrorCode;
+
+public class NotFoundException extends GeneralException {
+
+    public NotFoundException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
+    }
+
+    public NotFoundException(String message) {
+        super(message, ErrorCode.NOT_EXISTS);
+    }
+
+    public NotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
---
+++ src/main/java/capstone/carru/exception/advice/ControllerExceptionAdvice.java
@@ -0,0 +1,68 @@
+package capstone.carru.exception.advice;
+
+import static java.util.stream.Collectors.joining;
+
+import capstone.carru.driver.dto.ApiResponse;
+import capstone.carru.driver.dto.ErrorCode;
+import capstone.carru.exception.InvalidException;
+import capstone.carru.exception.NotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ControllerExceptionAdvice {
+
+    /**
+     * 400 Bad Request (잘못된 요청, Validation Exception)
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    private ApiResponse<Object> handleValidationError(BindException e) {
+        String errorMessage = e.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(joining("\n"));
+        return ApiResponse.error(ErrorCode.INVALID, errorMessage);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    private ApiResponse<Object> handleValidationParameterError(ConstraintViolationException e) {
+        String errorMessage = e.getMessage();
+        return ApiResponse.error(ErrorCode.INVALID, errorMessage);
+    }
+
+    /**
+     * 400 Bad Request (잘못된 요청)
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(InvalidException.class)
+    private ApiResponse<Object> handleBadRequest(InvalidException e) {
+        return ApiResponse.error(e.getErrorCode());
+    }
+
+    /**
+     * 404 Not Found (존재하지 않는 리소스)
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    private ApiResponse<Object> handleNotFound(NotFoundException e) {
+        return ApiResponse.error(e.getErrorCode());
+    }
+
+    /**
+     * 500 Internal Server Exception (서버 내부 에러)
+     */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    private ApiResponse<Object> handleInternalServerException(Exception e) {
+        return ApiResponse.error(ErrorCode.INTERNAL_SERVER, e.getMessage());
+    }
+}
+
