2c0969aecf17596606624678cfa414f10317fdaa
HanDaYeon-coder <hdy78901@naver.com>
2024-11-05 12:47:31 +0900

    feat : Spring Security 설정 및 JWT 토큰 구현

---
+++ src/main/java/capstone/carru/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package capstone.carru.config;
+
+import capstone.carru.exception.JwtAuthenticationEntryPoint;
+import capstone.carru.filter.JwtAuthenticationFilter;
+import capstone.carru.service.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )// JWT를 사용하기 때문에 세션을 사용하지 않음
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/v1/owner/sign-up", "/v1/driver/sign-up",
+                                        "/v1/driver/login", "/v1/owner/login", "/v1/user/sign-up",
+                                        "/v1/user/login",
+                                        "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
+                                .anyRequest().authenticated()
+                ).exceptionHandling(authenticationManager -> authenticationManager
+                                .authenticationEntryPoint(new JwtAuthenticationEntryPoint()))
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCrypt Encoder 사용
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
