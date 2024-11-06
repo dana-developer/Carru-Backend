a72d51c16034b762ea87730b656ade6e01f2fc58
HanDaYeon-coder <hdy78901@naver.com>
2024-11-06 21:38:01 +0900

    feat : 화물기사 물류 매칭 리스트 상세조회 기능 추가

--- src/main/java/capstone/carru/repository/product/ProductRepository.java
+++ src/main/java/capstone/carru/repository/product/ProductRepository.java
@@ -1,8 +1,9 @@
 package capstone.carru.repository.product;
 
 import capstone.carru.entity.Product;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
-
+    Optional<Product> findByIdAndDeletedDateIsNullAndApprovedDateIsNotNull(Long id);
 }
