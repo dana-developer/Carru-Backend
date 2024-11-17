fad4e9e93b795e74f30a7f652bd751ea577a94ed
HanDaYeon-coder <hdy78901@naver.com>
2024-11-17 11:25:30 +0900

    feat : StopOver repository 및 builder 생성

---
+++ src/main/java/capstone/carru/repository/stopOver/StopOverRepository.java
@@ -0,0 +1,8 @@
+package capstone.carru.repository.stopOver;
+
+import capstone.carru.entity.StopOver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StopOverRepository extends JpaRepository<StopOver, Long> {
+
+}
