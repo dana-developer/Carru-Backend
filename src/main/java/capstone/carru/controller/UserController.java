c4fae9530621bfdc90064a8cb8ce72ab90690a97
dana-developer <hdy78901@naver.com>
2024-11-20 15:28:11 +0900

    feat : 사용자 이름 변경

--- src/main/java/capstone/carru/controller/UserController.java
+++ src/main/java/capstone/carru/controller/UserController.java
@@ -5,11 +5,13 @@ import capstone.carru.dto.User.CreateUserRequest;
 import capstone.carru.dto.User.GetProfileResponse;
 import capstone.carru.dto.User.LoginRequest;
 import capstone.carru.dto.User.LoginResponse;
+import capstone.carru.dto.User.UpdateNameRequest;
 import capstone.carru.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,5 +45,14 @@ public class UserController {
     public ApiResponse<GetProfileResponse> loginUser(Authentication authentication) {
         String email = authentication.getName();
         return ApiResponse.success(userService.getProfile(email));
+    }
+
+    @Operation(summary = "이름 변경", description = "이름을 변경할 수 있습니다.")
+    @PatchMapping("/v1/user/name")
+    public ApiResponse<String> updateName(Authentication authentication,
+            @RequestBody UpdateNameRequest updateNameRequest) {
+        String email = authentication.getName();
+        userService.updateName(email, updateNameRequest);
+        return ApiResponse.success();
     }
 }
